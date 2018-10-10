83b7d414d2b740b6496a02c7cc0324ba2d5433db
aiswaryasiby <aiswaryasiby11@gmail.com>
2018-10-10 14:02:30 +0530

    committed

--- src/main/java/Add.java
+++ src/main/java/Add.java
@@ -1,7 +1,6 @@
-package addition;
+
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class Add extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	PrintWriter out;   
+       
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,11 +25,7 @@ public class Add extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		out= response.getWriter(); 
-		String num1=(request.getParameter("no1"));
-		String num2=(request.getParameter("no2"));
-		out.println(printAdd(num1,num2));
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
@@ -39,19 +34,6 @@ public class Add extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
-	
-	public String printAdd(String num1, String num2)
-	{
-		try{
-			float num3=Float.parseFloat(num1);
-			float num4=Float.parseFloat(num2);
-		    float result=num3+num4;
-		    return (Float.toString(result));
-	}
-	catch(Exception e){
-		return("please type int or float");
-	}
 	}
 
 }
