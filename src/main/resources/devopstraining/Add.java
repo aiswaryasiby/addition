2e740965b2001e0befe8b133ce10e0936b57ff66
aiswaryasiby <aiswaryasiby11@gmail.com>
2018-10-10 11:31:21 +0530

    added all the files

---
+++ src/main/resources/devopstraining/Add.java
@@ -0,0 +1,57 @@
+package devopstraining;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Add
+ */
+public class Add extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	PrintWriter out;   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Add() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		out= response.getWriter(); 
+		String num1=(request.getParameter("no1"));
+		String num2=(request.getParameter("no2"));
+		out.println(printAdd(num1,num2));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	public String printAdd(String num1, String num2)
+	{
+		try{
+			float num3=Float.parseFloat(num1);
+			float num4=Float.parseFloat(num2);
+		    float result=num3+num4;
+		    return (Float.toString(result));
+	}
+	catch(Exception e){
+		return("please type int or float");
+	}
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Addition of two numbers</title>
+</head>
+<body>
+
+<h1>Addition</h1>
+<form action="Add">
+<input type="text" name="no1">
+<input type="button" value="+">
+<input type="text" name="no2">
+<input type="submit" name="submit">
+</form>
+</body>
+</html>
